efa1c1d11ca51344db4d50a87d3aadee6e579d3b
sebi <gore.sebyx@yahoo.com>
2024-08-12 15:17:06 +0100

    turbo stream

--- lib/gem/ruby2html/render.rb
+++ lib/gem/ruby2html/render.rb
@@ -12,7 +12,7 @@ module Ruby2html
       kbd label legend li link main map mark meta meter nav noscript object ol optgroup option
       output p param picture pre progress q rp rt ruby s samp script section select small source
       span strong style sub summary sup table tbody td template textarea tfoot th thead time title
-      tr track u ul var video wbr turbo-frame
+      tr track u ul var video wbr turbo-frame turbo-stream
     ].freeze
 
     VOID_ELEMENTS = %w[area base br col embed hr img input link meta param source track wbr].freeze
